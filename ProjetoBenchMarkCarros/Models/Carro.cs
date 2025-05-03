bdce522343e0a5b14a28fcf98f0aa74d7f34fc4d
yorxcontreras <142018982+yorxcontreras@users.noreply.github.com>
2025-05-03 09:32:58 -0300

    Carro1

--- ProjetoBenchMarkCarros/Models/Carro.cs
+++ ProjetoBenchMarkCarros/Models/Carro.cs
@@ -7,6 +7,35 @@ namespace ProjetoBenchMarkCarros.Models
 {
     public class Carro
     {
+      
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        public decimal Valor { get; set; }
+        public int PotenciaCV { get; set; }
+        public float? ConsumoKmL { get; set; }  
+
+       
+        public Carro(string marca, string modelo, int ano, decimal valor, int potenciaCV, float? consumoKmL)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Ano = ano;
+            Valor = valor;
+            PotenciaCV = potenciaCV;
+            ConsumoKmL = consumoKmL;
+        }
+
         
+        public void ExibirDetalhes()
+        {
+            string consumoInfo = ConsumoKmL.HasValue ? $"{ConsumoKmL} km/L" : "N/A"; 
+            Console.WriteLine($"Marca: {Marca}");
+            Console.WriteLine($"Modelo: {Modelo}");
+            Console.WriteLine($"Ano: {Ano}");
+            Console.WriteLine($"Valor: R$ {Valor}");
+            Console.WriteLine($"Potência: {PotenciaCV} CV");
+            Console.WriteLine($"Consumo: {consumoInfo}");
+        }  
     }
 }
